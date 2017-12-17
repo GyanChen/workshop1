997bb1d096fee855cc422c8b511089af3c50b9bf
GyanChen <chenjiantx@163.com>
2017-12-17 21:04:20 +0800

    - should print welcome when game start

---
+++ src/test/java/com/ztesoft/zsmart/bss/cvbs/tdd/puzzle/GameProcessTest.java
@@ -0,0 +1,36 @@
+/**************************************************************************************** 
+ Copyright © 2003-2017 ZTEsoft Corporation. All rights reserved. Reproduction or       <br>
+ transmission in whole or in part, in any form or by any means, electronic, mechanical <br>
+ or otherwise, is prohibited without the prior written consent of the copyright owner. <br>
+ ****************************************************************************************/
+package com.ztesoft.zsmart.bss.cvbs.tdd.puzzle;
+
+import java.io.PrintStream;
+
+import org.mockito.Mockito;
+import org.junit.Test;
+
+/** 
+ * <Description> <br> 
+ *  
+ * @author chen.jian<br>
+ * @version 1.0<br>
+ * @taskId <br>
+ * @CreateDate 2017年12月17日 <br>
+ * @since V9.0C<br>
+ * @see com.ztesoft.zsmart.bss.cvbs.tdd.puzzle <br>
+ */
+
+public class GameProcessTest {
+    
+    @Test
+    public void should_print_welcome_when_game_start() {
+        PrintStream out = Mockito.mock(PrintStream.class);
+        GameProcess game = new GameProcess(out);
+        
+        Mockito.verify(out, Mockito.never()).println("Welcome!");
+        game.start();
+        Mockito.verify(out).println("Welcome!");
+    }
+
+}
